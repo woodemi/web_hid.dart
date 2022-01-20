e05b57603aec57b488ca8e777ed05167a978a1d9
wangkun <wangkun@woodemi.com>
2022-01-20 19:43:18 +0800

    Initial commit
    Signed-off-by: wangkun <wangkun@woodemi.com>

---
+++ example/web_hid_example.dart
@@ -0,0 +1,6 @@
+import 'package:web_hid/web_hid.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/web_hid.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library web_hid;
+
+export 'src/web_hid_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
